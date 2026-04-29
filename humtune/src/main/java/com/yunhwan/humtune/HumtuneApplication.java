940e72f6a10c7cbfb6807e89fdbf40165325afcc
YUNHWAN <629jyh7@naver.com>
2026-04-29 12:54:52 +0900

    feat: init-project

---
+++ humtune/src/main/java/com/yunhwan/humtune/HumtuneApplication.java
@@ -0,0 +1,13 @@
+package com.yunhwan.humtune;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumtuneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumtuneApplication.class, args);
+	}
+
+}
