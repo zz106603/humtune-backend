eb46b3d70cb254367ea35e01f7c6bb4b70e93106
YUNHWAN <629jyh7@naver.com>
2026-04-29 12:59:17 +0900

    feat: /helth 엔드포인트 추가

--- humtune/src/test/java/com/yunhwan/humtune/HumtuneApplicationTests.java
+++ humtune/src/test/java/com/yunhwan/humtune/HumtuneApplicationTests.java
@@ -1,13 +1,32 @@
 package com.yunhwan.humtune;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class HumtuneApplicationTests {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void healthReturnsOk() {
+		var response = restTemplate.getForEntity("http://localhost:" + port + "/health", String.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(response.getBody()).isEqualTo("OK");
+	}
 }
