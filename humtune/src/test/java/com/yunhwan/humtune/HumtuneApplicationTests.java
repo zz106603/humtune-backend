5b5d34df3aca6c56c00530a51319aa827ab5cada
YUNHWAN <629jyh7@naver.com>
2026-04-29 12:54:52 +0900

    feat: init-project

---
+++ humtune/src/test/java/com/yunhwan/humtune/HumtuneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunhwan.humtune;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumtuneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
