8292dbaf814dcad23d14b24def13b4e63e90a741
YUNHWAN <629jyh7@naver.com>
2026-05-07 13:20:12 +0900

    feat: 이벤트 리스너 및 프로세서 추가

---
+++ src/main/java/com/yunhwan/humtune/application/AnalysisRequestedEvent.java
@@ -0,0 +1,4 @@
+package com.yunhwan.humtune.application;
+
+public record AnalysisRequestedEvent(Long analysisRequestId) {
+}
---
+++ src/main/java/com/yunhwan/humtune/application/AudioAnalysisEventListener.java
@@ -0,0 +1,22 @@
+package com.yunhwan.humtune.application;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Component
+public class AudioAnalysisEventListener {
+
+	private final AudioAnalysisProcessor audioAnalysisProcessor;
+
+	public AudioAnalysisEventListener(AudioAnalysisProcessor audioAnalysisProcessor) {
+		this.audioAnalysisProcessor = audioAnalysisProcessor;
+	}
+
+	@Async
+	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+	public void handle(AnalysisRequestedEvent event) {
+		audioAnalysisProcessor.process(event.analysisRequestId());
+	}
+}
