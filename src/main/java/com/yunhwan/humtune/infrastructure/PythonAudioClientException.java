1f6f214b0326c5ca14bebb0da71ae7106b82e4f9
YUNHWAN <629jyh7@naver.com>
2026-05-08 09:38:20 +0900

    feat: audio client exeption 추가

---
+++ src/main/java/com/yunhwan/humtune/infrastructure/PythonAudioClientException.java
@@ -0,0 +1,8 @@
+package com.yunhwan.humtune.infrastructure;
+
+public class PythonAudioClientException extends RuntimeException {
+
+	public PythonAudioClientException(String message) {
+		super(message);
+	}
+}
