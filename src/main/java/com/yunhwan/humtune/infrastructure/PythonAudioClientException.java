1360fdec0d472b87778373c6eeb55dbfb52ec3f8
YUNHWAN <629jyh7@naver.com>
2026-05-08 10:09:15 +0900

    feat: Failure Category 추가

--- src/main/java/com/yunhwan/humtune/infrastructure/PythonAudioClientException.java
+++ src/main/java/com/yunhwan/humtune/infrastructure/PythonAudioClientException.java
@@ -2,11 +2,34 @@ package com.yunhwan.humtune.infrastructure;
 
 public class PythonAudioClientException extends RuntimeException {
 
+	public enum FailureCategory {
+		PYTHON_HTTP_ERROR,
+		PYTHON_TIMEOUT,
+		PYTHON_NETWORK_ERROR,
+		PYTHON_CLIENT_ERROR
+	}
+
+	private final FailureCategory failureCategory;
+
 	public PythonAudioClientException(String message) {
-		super(message);
+		this(FailureCategory.PYTHON_CLIENT_ERROR, message);
 	}
 
 	public PythonAudioClientException(String message, Throwable cause) {
+		this(FailureCategory.PYTHON_CLIENT_ERROR, message, cause);
+	}
+
+	public PythonAudioClientException(FailureCategory failureCategory, String message) {
+		super(message);
+		this.failureCategory = failureCategory;
+	}
+
+	public PythonAudioClientException(FailureCategory failureCategory, String message, Throwable cause) {
 		super(message, cause);
+		this.failureCategory = failureCategory;
+	}
+
+	public FailureCategory getFailureCategory() {
+		return failureCategory;
 	}
 }
